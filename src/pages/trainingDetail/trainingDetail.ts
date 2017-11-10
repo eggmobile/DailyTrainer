fcf33cea000c879e81cc485b35b92f348926ccf7
Roh <soonyung.roh@accenture.com>
2017-11-10 17:55:47 +0900

    fix

--- src/pages/trainingDetail/trainingDetail.ts
+++ src/pages/trainingDetail/trainingDetail.ts
@@ -6,6 +6,11 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'trainingDetail.html'
 })
 export class TrainingDetailPage {
+featureTitle = "leg"; //部位名
+detailTitle = "course 1" ; //詳細コース名
+instructionImg; 
+instructionYouTube;
+description;
 
   constructor(public navCtrl: NavController) {
 
