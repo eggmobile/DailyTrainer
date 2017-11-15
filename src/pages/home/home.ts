f02fae811f1ac4f1e68d1918eed36e151ad70459
DIR\shin.kubota <shin.kubota@accenture.com>
2017-11-15 16:40:32 +0900

    homeの編集

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { MenuDetailPage } from '../menuDetail/menuDetail';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { FirebaseProvider } from './../../providers/firebase/firebase';
+
 
 @Component({
   selector: 'page-home',
@@ -12,9 +19,9 @@ export class HomePage {
   isHome; // 変数定義
   featureArray;
   trainedTimes;
+  featuresName: Observable<any[]>;
 
-
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public afd: AngularFireDatabase, public firebaseProvider: FirebaseProvider) {
     this.isHome = "test";
     this.featureArray = [
       {featureTitle: "Arm", featureImg: "", trainedTimes: 1},
@@ -22,6 +29,12 @@ export class HomePage {
       {featureTitle: "Abs", featureImg: "", trainedTimes: 3},
       {featureTitle: "Leg", featureImg: "", trainedTimes: 4}
     ];
+
+    this.featuresName = this.firebaseProvider.getFeaturesItems();
+  }
+
+  getFeaturesName() {
+    return this.afd.list('features').valueChanges();
   }
 
   goToMenuDetailPage() {
